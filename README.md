69fe87efabb46547de61e313038db833eb18814d
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-03-13 10:58:13 -0700

    finished header, stuck on tabs and cards

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ A Component is code that allows us to create repeat elements, rather than having
 
 Your finished project must include all of the following requirements:
 
-* [ ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the HTML file itself.
+* [x] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the HTML file itself.
 
 * [ ] Following the instructions in the `Header/index.js` file, create the Header component. 
 
--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,35 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function articleCards(info) {
+    const card = document.createElement('div')
+        const headline = document.createElement('div')
+        const author = document.createElement('div')
+            const imgContainer = document.createElement('div')
+                const img = document.createElement('img')
+            const authorsName = document.createElement('span')
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    headline.textContent = info.headline
+    
+    img.src = info.authorPhoto
+
+    authorsName.textContent = info.authorName
+
+    return card
+}
+
+// axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+// .then(response => {
+//     console.log(response.data.articles.javascript)
+//     const cardsContainer = document.querySelector('.cards-container')
+//     cardsContainer.appendChild(articleCards(response.data.articles))
+// })
+// .catch(error => {
+//     console.log('whoops', error)
+// })
